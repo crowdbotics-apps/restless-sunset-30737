c78e71850cb1b789c3a933178ae9a93d679db729
Crowdbotics <team@crowdbotics.com>
2021-09-22 02:35:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "restless_sunset_30737Identifier",
+  url: "https://restless_sunset_30737Identifier.botics.co",
+  api: "https://restless_sunset_30737Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
